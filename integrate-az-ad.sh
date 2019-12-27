08f79dd430eb9ce46382b959b17f4c1336b11fc8
asegni <segniaymen1@gmail.com>
2019-12-27 21:16:39 +0100

    Create aad server

--- integrate-az-ad.sh
+++ integrate-az-ad.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# Provide your own unique aksname within the Azure AD tenant
+aksname="run-it-on-cloud"
+resourcegroup="runItOnCloud"
+location="westeurope"
 
+
+# Create the Azure AD application Server
+serverApplicationId=$(az ad app create \
+    --display-name "${aksname}Server" \
+    --identifier-uris "https://${aksname}Server" \
+    --query appId -o tsv)
+
+# Update the application group memebership claims
+az ad app update --id $serverApplicationId --set groupMembershipClaims=All
