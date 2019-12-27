c00d365f564c1fdae0aead0e569f95ea93d0b717
asegni <segniaymen1@gmail.com>
2019-12-27 22:16:43 +0100

    Integrate aad with aks

--- integrate-az-ad.sh
+++ integrate-az-ad.sh
@@ -13,3 +13,41 @@ serverApplicationId=$(az ad app create \
 
 # Update the application group memebership claims
 az ad app update --id $serverApplicationId --set groupMembershipClaims=All
+
+# Create a service principal for the Azure AD application
+az ad sp create --id $serverApplicationId
+
+# Get the service principal secret
+serverApplicationSecret=$(az ad sp credential reset \
+    --name $serverApplicationId \
+    --credential-description "AKSPassword" \
+    --query password -o tsv)
+# The Azure AD needs permissions to perform the following actions:
+# 1-Read directory data
+# 2-Sign in and read user profile
+
+az ad app permission add \
+    --id $serverApplicationId \
+    --api 00000003-0000-0000-c000-000000000000 \
+    --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope 06da0dbc-49e2-44d2-8312-53f166ab848a=Scope 7ab1d382-f21e-4acd-a863-ba3e13f7da61=Role
+# grant the permissions assigned in the previous step for the server application 
+# You must be the Azure AD tenant admin for these steps to successfully complete
+az ad app permission grant --id $serverApplicationId --api 00000003-0000-0000-c000-000000000000
+az ad app permission admin-consent --id  $serverApplicationId
+
+# Create the Azure AD application Client 
+clientApplicationId=$(az ad app create \
+    --display-name "${aksname}Client" \
+    --native-app \
+    --reply-urls "https://${aksname}Client" \
+    --query appId -o tsv)
+
+# Create a service principal for the client application
+az ad sp create --id $clientApplicationId
+
+# Get the oAuth2 ID for the server app to allow authentication flow
+oAuthPermissionId=$(az ad app show --id $serverApplicationId --query "oauth2Permissions[0].id" -o tsv)
+
+# Assign permissions for the client and server applications to communicate with each other
+az ad app permission add --id $clientApplicationId --api $serverApplicationId --api-permissions $oAuthPermissionId=Scope
+az ad app permission grant --id $clientApplicationId --api $serverApplicationId
